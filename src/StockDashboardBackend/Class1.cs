065015ff7382fa8e78b29badee8073205263801c
diogomota <dmk998@gmailcom>
2019-12-16 22:07:28 +0000

    Initial setup

---
+++ src/StockDashboardBackend/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace StockDashboardBackend
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/StockDashboardBackend/FundamentalAnalysis/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace StockDashboardBackend
+{
+    public static class FundamentalAnalysis
+    {
+        
+    }
+}
