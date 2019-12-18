e4f69af7c1601032d221c2b7e082a49d65f4ef39
diogomota <dmk998@gmailcom>
2019-12-18 22:20:00 +0000

    Added FundamentalAnalysis results dto; Included MSFt extensions nuget to use HTTPClientFactory

--- src/StockDashboardBackend/FundamentalAnalysis/FundamentalAnalysisResults.cs
+++ src/StockDashboardBackend/FundamentalAnalysis/FundamentalAnalysisResults.cs
@@ -2,8 +2,37 @@
 
 namespace StockDashboardBackend
 {
-    public static class FundamentalAnalysisResults
+
+    // Class with the final object that is serialized and passed as json to the frontend
+    // (i.e: dto for Fundamental analysis data)
+    public class FundamentalAnalysisResults
     {
-        
+        public double SharePrice {get;}
+
+        public double ThreeMonthGrowth {get;}
+
+        public double ROTA {get;}
+
+        public double ROE {get;}
+
+        public double DebtToEquity {get;}
+
+        public double CurrentRatio {get;}
+
+        public double GrahamSharePrice {get;}
+
+        public FundamentalAnalysisResults(double _shareP, double _threeMonthGrowth,
+                                            double _rota, double _roe,
+                                            double _debtToEquity, double _currentRatio, 
+                                            double _grahamSharePrice)
+        {
+            this.SharePrice = _shareP;
+            this.ThreeMonthGrowth = _threeMonthGrowth;
+            this.ROTA = _rota;
+            this.ROTA = _roe;
+            this.DebtToEquity = _debtToEquity;
+            this.CurrentRatio = _currentRatio;
+            this.GrahamSharePrice = _grahamSharePrice;
+        }
     }
 }
